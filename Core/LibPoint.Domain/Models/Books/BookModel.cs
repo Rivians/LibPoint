4ca664b911dd799b35add60a3df14477bbe8cdb2
offermade <kaaneris3@hotmail.com>
2025-06-01 01:12:24 +0300

    added model of book

---
+++ Core/LibPoint.Domain/Models/Books/BookModel.cs
@@ -0,0 +1,22 @@
+﻿using LibPoint.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibPoint.Domain.Models.Books
+{
+    public class BookModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ISBN { get; set; } // kitap numarası
+        public bool IsAvailable { get; set; } = true;
+        public string? Publisher { get; set; }
+        public int? PublishedYear { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+
+    }
+}
